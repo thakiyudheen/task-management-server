83f5bc7c68575b9efe8ad163444951f78f5102d8
thakiyudheen <thakeyudheen525@gmail.com>
2024-08-01 09:13:20 +0530

    add versel cors

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 // CORS configuration
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin:'https://task-management-client.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
   credentials: true,
 };
